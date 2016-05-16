8e3d6609ac068ee6638203442d872e747a609915
Alsou <salsou96@mail.ru>
2016-05-16 10:41:23 +0300

    Add files via upload

--- WindowsFormsApplication3/WindowsFormsApplication3/Form1.cs
+++ WindowsFormsApplication3/WindowsFormsApplication3/Form1.cs
@@ -26,6 +26,7 @@ namespace WindowsFormsApplication3
 
         private async void button1_Click(object sender, EventArgs e)
         {
+            listBox1.Items.Clear();
             var ghe = new Uri("https://github.com/");
             client = new GitHubClient(new ProductHeaderValue(textBox1.Text), ghe);
 
@@ -35,46 +36,41 @@ namespace WindowsFormsApplication3
             foreach (var repository in repositories)
             {
                 data = repository.UpdatedAt;
-                listBox1.Items.Add("Name " + repository.Name);
-                listBox1.Items.Add("Update " + data.ToString());
-                listBox1.Items.Add("");
+                listBox1.Items.Add("Name: " + repository.Name);
+                //listBox1.Items.Add("Update " + data.ToString());
+                //listBox1.Items.Add("");
             }
             
         }
 
         private async void button2_Click(object sender, EventArgs e)
         {
+            listBox2.Items.Clear();
             var ghe = new Uri("https://github.com/");
             var client = new GitHubClient(new ProductHeaderValue(textBox1.Text), ghe);
 
             var user = await client.User.Get(textBox1.Text);
            
-            listBox2.Items.Add("Name " +  user.Name);
-            listBox2.Items.Add("Email " + user.Email);
+            listBox2.Items.Add("Name: " +  user.Name);
+            listBox2.Items.Add("Email: " + user.Email);
         }
 
 
         private async void listBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            foreach (var rep in repositories)
+            listBox3.Items.Clear();
+            var rep = repositories[listBox1.SelectedIndex];
+            var commits = await client.Repository.Commits.GetAll(rep.Owner.Login, rep.Name);
+
+            listBox3.Items.Add("Репозиторий: " + rep.Name);
+            listBox3.Items.Add("");
+
+            //var s = rep.Url;
+            foreach (var c in commits)
             {
-                if (listBox1.SelectedItem.ToString() == "Name " + rep.Name)
-                {
-                    var commits = await client.Repository.Commits.GetAll(rep.Owner.Login, rep.Name);
-
-                    listBox3.Items.Add(rep.Name);
-
-                    var s = rep.Url;
-                    foreach (var c in commits)
-                    {
-                        listBox3.Items.Add(c.Committer.Login + " " + c.Commit.Author.Date.ToString());
-                        listBox3.Items.Add(c.Commit.Message);
-
-                    }
-                    listBox3.Items.Add("");
-                }
-                
-            } 
+                listBox3.Items.Add("Коммитер: " + c.Committer.Login + ", время: " + c.Commit.Author.Date.ToString());
+                listBox3.Items.Add("Изменения: " + c.Commit.Message);
+            }                
         }
     }
 }
